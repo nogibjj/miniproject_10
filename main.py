484059763b82a60e2fbe65e32aa3977194296e7a
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-08 01:21:10 +0000

    Week_2 homework

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-def main(input_list):
-    return len(input_list)
+import pandas as pd
+
+
+def pandas_descriptive_stat_mean(df: pd.DataFrame, col: str) -> float:
+    return df[col].mean()
+
+if __name__ == '__main__':
+    cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+    print(pandas_descriptive_stat_mean(cars, 'mpg'))
--- test_main.py
+++ test_main.py
@@ -1,8 +1,14 @@
-from main import main
-def test_max():
-    """Function calling listMax"""
+from main import pandas_descriptive_stat_mean
+import pandas as pd
 
-    list1 = [1, 2, 3, 4, 5, 6]
-    list2 = [1, 2, 3, 4]
-    assert main(list1) == 6
-    assert main(list2) == 4
+def test_pandas_descriptive_stat_mean():
+    cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+    target_column = "mpg"
+    mean_mpg = pandas_descriptive_stat_mean(cars, target_column)
+
+    calculated_mean = cars[target_column].sum()/len(cars[target_column])
+    assert mean_mpg == calculated_mean
+
+
+if __name__ == "__main__":
+    test_pandas_descriptive_stat_mean()
